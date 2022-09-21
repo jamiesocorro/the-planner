1497b191d476467fe144709ebe263b8ec8b65ad2
Jamie Socorro <jamie_socorro@live.com>
2022-09-21 17:36:36 +0800

    removed modules on app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopbarComponent } from './shared/components/topbar/topbar.component';
-import { PlanningModule } from './modules/planning/planning.module';
-import { EmployeesModule } from './modules/employees/employees.module';
-import { ShiftTemplatesModule } from './modules/shift-templates/shift-templates.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, TopbarComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    PlanningModule,
-    EmployeesModule,
-    ShiftTemplatesModule,
-  ],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
